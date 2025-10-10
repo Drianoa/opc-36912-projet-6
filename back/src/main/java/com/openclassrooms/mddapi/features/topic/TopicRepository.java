dc167603f1891897beac8887ef144a9430666716
Drianoa <1522877+drianoa@users.noreply.github.com>
2025-10-10 23:20:03 +0200

    feat: enhance topic retrieval with user count and sorting

--- back/src/main/java/com/openclassrooms/mddapi/features/topic/TopicRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/features/topic/TopicRepository.java
@@ -15,11 +15,13 @@ public interface TopicRepository extends JpaRepository<Topic, Long> {
             t.id AS id,
             t.name AS name,
             t.description AS description,
-            CASE WHEN (COUNT(u.id) > 0) THEN true ELSE false END AS subscribed
+            CASE WHEN (COUNT(u.id) > 0) THEN true ELSE false END AS subscribed,
+            COUNT(u.id) AS userCount
         FROM Topic t
         LEFT JOIN t.users u
         WITH u.email = :email
         GROUP BY t.id, t.name, t.description
+        ORDER BY userCount ASC , t.name ASC
         """)
     List<UserTopicDto> findTopicsBySubscriptionStatus(String email);
 }
