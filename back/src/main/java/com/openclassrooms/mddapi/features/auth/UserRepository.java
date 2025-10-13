122465e2ba7e81dcaad327804eef596d66aac71d
Drianoa <1522877+drianoa@users.noreply.github.com>
2025-10-13 21:00:41 +0200

    feat(back): add update user api

--- back/src/main/java/com/openclassrooms/mddapi/features/auth/UserRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/features/auth/UserRepository.java
@@ -4,11 +4,16 @@ import com.openclassrooms.mddapi.model.User;
 import java.util.Collection;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findUserByEmailOrUsername(String email, String username);
 
     Optional<User> findUserByUsernameIsInOrEmailIsIn(Collection<String> usernames, Collection<String> emails);
+
+    @Query("SELECT u FROM User u WHERE (u.username IN :usernames OR u.email IN :emails) AND u.id <> :userId")
+    Optional<User> findUserByUsernameIsInOrEmailIsInAndIdIsNot(
+            Collection<String> usernames, Collection<String> emails, Integer userId);
 
     Optional<User> findUserByEmail(String email);
 }
