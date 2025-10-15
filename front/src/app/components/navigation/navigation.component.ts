f50c3592cce8efb86b5bd6ad977c42bf7f62b88f
Drianoa <1522877+drianoa@users.noreply.github.com>
2025-10-15 23:28:35 +0200

    feat: navigation drawer component in tailwind

--- front/src/app/components/navigation/navigation.component.ts
+++ front/src/app/components/navigation/navigation.component.ts
@@ -1,14 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
-import {AsyncPipe} from '@angular/common';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
-import {DomSanitizer} from '@angular/platform-browser';
-import {Observable} from 'rxjs';
-import {map, shareReplay} from 'rxjs/operators';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {SessionService} from "../../core/services/session.service";
 import {PostsService} from "../../core/services/posts.service";
@@ -19,12 +9,6 @@ import {BackButtonComponent} from "../back-button/back-button.component";
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
   imports: [
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
-    AsyncPipe,
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
@@ -32,26 +16,10 @@ import {BackButtonComponent} from "../back-button/back-button.component";
   ]
 })
 export class NavigationComponent implements OnInit {
-  private breakpointObserver = inject(BreakpointObserver);
-  private iconRegistry = inject(MatIconRegistry);
-  private sanitizer = inject(DomSanitizer);
   sessionService = inject(SessionService)
   router = inject(Router)
   postsService = inject(PostsService)
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.HandsetPortrait)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-
-  constructor() {
-    // Enregistrer l'icône utilisateur SVG personnalisée
-    this.iconRegistry.addSvgIcon(
-      'mdd-user',
-      this.sanitizer.bypassSecurityTrustResourceUrl('assets/user.svg')
-    );
-  }
+  isOpen = signal(false)
 
 
   ngOnInit(): void {
@@ -65,7 +33,7 @@ export class NavigationComponent implements OnInit {
       }, error: (err) => {
         console.error('Error fetching posts on navigation init:', err);
       }
-    })
+    });
   }
 
   logout() {
@@ -76,5 +44,13 @@ export class NavigationComponent implements OnInit {
   showBackButton(): boolean {
     const currentUrl = this.router.url;
     return currentUrl !== '/' && currentUrl !== '/posts' && currentUrl !== '/topics' && currentUrl !== '/me';
+  }
+
+  open() {
+    this.isOpen.set(true);
+  }
+
+  close() {
+    this.isOpen.set(false);
   }
 }
