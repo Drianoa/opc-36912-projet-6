a2a7a2c27687ff556d4324e8aa7133a9ffe93796
Drianoa <1522877+drianoa@users.noreply.github.com>
2025-10-20 16:55:30 +0200

    fix(infrastructure): Configure MySQL for UTF8MB4 character set and collation

--- infrastructure/script.sql
+++ infrastructure/script.sql
@@ -1,3 +1,9 @@
+SET NAMES utf8mb4;
+SET character_set_client = utf8mb4;
+SET character_set_connection = utf8mb4;
+SET character_set_results = utf8mb4;
+SET collation_connection = utf8mb4_unicode_ci;
+
 CREATE TABLE `users`
 (
     `id`         integer PRIMARY KEY AUTO_INCREMENT,
