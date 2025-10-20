99ebece33640a60eb30a4b317bc1a68e0a93ff32
Drianoa <1522877+drianoa@users.noreply.github.com>
2025-10-20 22:47:28 +0200

    refactor(components): remove unused imports and add keyboard support

--- front/src/app/components/navigation/navigation.component.ts
+++ front/src/app/components/navigation/navigation.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import {
-  NavigationEnd,
   Router,
   RouterLink,
   RouterLinkActive,
@@ -8,8 +7,6 @@ import {
 } from '@angular/router';
 import { SessionService } from '../../core/services/session.service';
 import { PostsService } from '../../core/services/posts.service';
-import { filter, takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
